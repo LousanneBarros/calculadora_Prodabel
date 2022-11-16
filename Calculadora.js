fea868078662578c348f2bf7f33183257ccf3be2
Lousanne Barros <49535785+Loubarros@users.noreply.github.com>
2022-11-16 09:58:01 -0300

    comite do projeto calculadora

---
+++ Calculadora.js
@@ -0,0 +1,25 @@
+class Calculadora {
+    sumar(num1, num2) {
+        return num1 + num2;
+    }
+
+    restar(num1, num2) {
+        return num1 - num2;
+    }
+
+    dividir(num1, num2) {
+        return num1 / num2;
+    }
+
+    multiplicar(num1, num2) {
+        return num1 * num2;
+    }
+
+    porcentagem(num1, num2) {
+        return (num1 * num2) / 100;
+    }
+    
+    potenciacao(num1, num2) {
+        return (num1 ** num2)
+    }
+} 
